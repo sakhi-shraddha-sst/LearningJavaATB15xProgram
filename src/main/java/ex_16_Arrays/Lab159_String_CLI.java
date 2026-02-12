65929c5a9f68d7c2385ae7b36ea4c130c4e9f09d
sakhi-shraddha-sst <shraddha.st.qa@gmail.com>
2026-02-12 15:13:44 +0530

    Programs till 16th Jan 26

--- src/main/java/ex_16_Arrays/Lab159_String_CLI.java
+++ src/main/java/ex_16_Arrays/Lab159_String_CLI.java
@@ -1,6 +1,6 @@
 package ex_16_Arrays;
 
-public class Lab160_String_CLI {
+public class Lab159_String_CLI {
     public static void main(String[] args) {
         for(int i = 0; i < args.length; i++){
             System.out.println(args[i]);
--- src/main/java/task/Jan16_2nd_Highest_Number.java
+++ src/main/java/task/Jan16_2nd_Highest_Number.java
@@ -1,13 +1,9 @@
-package ex_16_Arrays;
+package task;
 
-import java.util.Arrays;
-
-public class Lab159_Interview_2nd_Highest_Number {
+public class Jan16_2nd_Highest_Number {
     public static void main(String[] args) {
+        // Find out the 2nd highest number in the array
         int[] numbers = {12, 34, 10, 1, 100, 3, 4, 32}; // 100, 34
-
-//        Arrays.sort(numbers);
-//        System.out.println("2nd highest number : " + numbers[numbers.length-2]);
 
         int highest = 0;
         int second_highest = 0;
@@ -17,11 +13,11 @@ public class Lab159_Interview_2nd_Highest_Number {
             if(numbers[i] > highest) {
                 second_highest = highest;
                 highest = numbers[i];
-                }
+            }
             else if(numbers[i] > second_highest && numbers[i] != highest){
                 second_highest = numbers[i];
             }
-            }
+        }
         System.out.println("Highest number : " + highest);
 
         System.out.println("2nd highest number : " + second_highest);
